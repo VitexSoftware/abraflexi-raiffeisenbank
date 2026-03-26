4f49966b607abcbb836328da7dbcbf2b55b3baad
CyberVitexus <info@vitexsoftware.cz>
2026-03-26 10:05:13 +0100

    fix: correct file paths in autoload and install scripts for consistency

--- debian/autoload.php
+++ debian/autoload.php
@@ -1,11 +1,14 @@
 <?php
+
 // Debian autoloader for abraflexi-raiffeisenbank
 require_once '/usr/share/php/AbraFlexi/autoload.php';
 require_once '/usr/share/php/Raiffeisenbank/autoload.php';
 // PSR-4 autoloader for application classes
 spl_autoload_register(function (string $class): void {
-    if (strncmp('AbraFlexi\\RaiffeisenBank\\', $class, 26) === 0) {
-        $file = '/usr/lib/abraflexi-raiffeisenbank/AbraFlexi/RaiffeisenBank/' . str_replace('\\', '/', substr($class, 26)) . '.php';
-        if (file_exists($file)) { require $file; }
+    if (strncmp('AbraFlexi\\RaiffeisenBank\\', $class, 25) === 0) {
+        $file = 'usr/share/php/AbraFlexi/RaiffeisenBank/' . str_replace('\\', '/', substr($class, 25)) . '.php';
+        if (file_exists($file)) {
+            require $file;
+        }
     }
 });
