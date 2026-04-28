d5862a1670c24249e3eddc04b72e7f44c84e4798
Vítězslav Dvořák <vitezslav.dvorak@spojenet.cz>
2026-04-28 16:48:58 +0200

    update for current multfilexi-cli syntax

--- src/setup.php
+++ src/setup.php
@@ -17,7 +17,6 @@ require_once '../vendor/autoload.php';
 /**
  * Get List of bank accounts and import it into AbraFlexi.
  */
-
 $options = getopt('e::', ['environment::']);
 \Ease\Shared::init(
     ['ABRAFLEXI_URL', 'ABRAFLEXI_LOGIN', 'ABRAFLEXI_PASSWORD', 'ABRAFLEXI_COMPANY', 'CERT_FILE', 'CERT_PASS', 'XIBMCLIENTID', 'ACCOUNT_NUMBER'],
